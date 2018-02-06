79c053befab406d78e64177d6ac98e147970df1e
Nick Darvey <nickdarvey@outlook.com.au>
2018-02-06 21:06:11 +1100

    Adds more tests for relative URIs

--- ServiceFabric.ServiceUris.UnitTests/ReverseProxyTests.cs
+++ ServiceFabric.ServiceUris.UnitTests/ReverseProxyTests.cs
@@ -17,10 +17,12 @@ namespace NickDarvey.ServiceFabric.ServiceUris.UnitTests
             Assert.Equal(new Uri("https://localhost:19080/Application/Name/And/Service/Name"), result);
         }
 
-        [Fact]
-        public void ToReverseProxy_Returns_ServiceUri_When_Relative()
+        [Theory]
+        [InlineData("/Application/Name/And/Service/Name")]
+        [InlineData("Application/Name/And/Service/Name")]
+        public void ToReverseProxy_Returns_ServiceUri_When_Relative(string sourceServiceUri)
         {
-            var original = new Uri("/Application/Name/And/Service/Name", UriKind.Relative);
+            var original = new Uri(sourceServiceUri, UriKind.Relative);
 
             var result = original.ToReverseProxyServiceUri(new Uri("https://localhost:19080"));
 
